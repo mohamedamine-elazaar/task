b346000e616f9bf27047103c238cff058afbb591
mohamedamine-elazaar <dev.mohamed.amine@gmail.com>
2025-11-25 16:32:22 +0100

    env

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_TARGET || 'http://localhost:5000',
+          changeOrigin: true,
+        },
       },
     },
-  },
+  }
 })
